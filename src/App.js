07da68e68dced5fca71cd3690684da2bcbaabf3b
RohanK22 <rohank2205@gmail.com>
2021-05-20 19:38:38 +0530

    Borrowed check for win function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from 'react';
 function App() {
 	// State to store 3x3 grid
 	const [board, setBoard] = useState(Array(9).fill(null));
+  const []
+
 	const boardGUI = () => {
 		return (
 			<div className="board">
@@ -19,6 +21,27 @@ function App() {
 	};
 
 	return <div className="App">{boardGUI()}</div>;
+}
+
+// Check for win function : Thanks to https://www.freecodecamp.org/news/learn-how-to-build-tic-tac-toe-with-react-hooks/
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
 }
 
 export default App;
