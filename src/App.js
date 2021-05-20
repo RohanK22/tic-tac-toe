47ed11a63309f94d67343fee6eaf3cf383164c4f
RohanK22 <rohank2205@gmail.com>
2021-05-20 17:17:35 +0530

    clicker

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+	const [count, setCount] = useState(0);
+	// Another example
+  const [todos, setTodos] = useState([
+		{
+			text: 'Learn Hooks!',
+		},
+	]);
 
-  
-	return <div className="App">This is an App!</div>;
+	return (
+		<div className="App">
+			<p>You clicked {count} times</p>
+			<button
+				onClick={() => {
+					return setCount(count + 1);
+				}}
+			>
+				{' '}
+				Click Me!{' '}
+			</button>
+		</div>
+	);
 }
 
 export default App;
